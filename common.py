b0b564ae7633300bb77c68055decb2b44b7f8fc2
Adam Novak <interfect@gmail.com>
2026-04-13 20:02:06 -0400

    Look for frames that wander around and drop them as ads

--- common.py
+++ common.py
@@ -8,44 +8,69 @@ import sys
 
 from mp3 import good_data
 
-def get_common_chunks(in_streams, out_stream):
+def get_common_chunks(in_streams, out_stream, max_ad_load=0.20):
     """
-    Write only tag blocks and frames common to all the input MP3 streams to the output MP3 stream.
+    Write only tag blocks and frames common to all the input MP3 streams, without too much variation in their loction, to the output MP3 stream.
+
+    Needs to know the maximum plausible ad load in fraction of tag blocks and frames, to identify ads by location shift.
     """
+
+    # TODO: Real alignment or kmer de Bruijn graph to remove frames in ads that also exist in content (like silence).
 
     if len(in_streams) == 0:
         return
 
-    # Collect all ID3 blocks and frames from the first file as a set
-    wanted_chunks = set()
+    # Collect all ID3 blocks and frames as a dict from possibly-wanted chunk to first observed chunk number list
+    wanted_chunks = {}
 
     if len(in_streams) > 1:
-        for chunk in good_data(in_streams[0]):
-            wanted_chunks.add(chunk)
+        for i, chunk in enumerate(good_data(in_streams[0])):
+            if chunk not in wanted_chunks:
+                # Track only first appearance
+                wanted_chunks[chunk] = [i]
         sys.stderr.write(f"First stream contains {len(wanted_chunks)} frames and tag blocks\n")
 
+    representative_length = len(wanted_chunks)
+
     if len(in_streams) > 2:
-        for intermediate_stream in in_streams[1:-1]:
-            wanted_chunks_observed = set()
-            for chunk in good_data(intermediate_stream):
+        for stream_num in range(1, len(in_streams) - 1):
+            intermediate_stream = in_streams[stream_num]
+            for i, chunk in enumerate(good_data(intermediate_stream)):
                 # Keep wanting only the chunks also in this stream
-                if chunk in wanted_chunks:
-                    wanted_chunks_observed.add(chunk)
-            sys.stderr.write(f"After intermediate stream, {len(wanted_chunks_observed)}/{len(wanted_chunks)} chunks are still wanted\n")
-            wanted_chunks = wanted_chunks_observed
+                if chunk in wanted_chunks and len(wanted_chunks[chunk]) < stream_num + i:
+                    # Track only first appearance
+                    wanted_chunks[chunk].append(i)
+            # Drop any chunks that didn't appear in all streams so far
+            wanted_chunks = {k: v for k, v in wanted_chunks.items() if len(v) == stream_num + 1}
 
-
+            sys.stderr.write(f"After middle stream {stream_num}, {len(wanted_chunks)} chunks are still wanted\n")
 
     dropped = 0
     total = 0
-    for chunk in good_data(in_streams[-1]):
+    for i, chunk in enumerate(good_data(in_streams[-1])):
         total += 1
-        if len(in_streams) > 1 and chunk not in wanted_chunks:
-            dropped += 1
-            continue
+        if len(in_streams) > 1:
+            if chunk not in wanted_chunks:
+                dropped += 1
+                continue
+            if len(wanted_chunks[chunk]) < len(in_streams):
+                # Track only first appearance
+                wanted_chunks[chunk].append(i)
+            # Work out  the range of first appearance chunk numbers for this chunk
+            min_location = min(wanted_chunks[chunk])
+            max_location = max(wanted_chunks[chunk])
+            location_shift = (max_location - min_location) / representative_length
+            if location_shift > max_ad_load:
+                # This frame appeared in everything but at a wildly variable location: probably an ad
+                sys.stderr.write(f"Dropping common frame or tag block that ranged in first position from {min_location} to {max_location}\n")
+                dropped += 1
+                continue
+
         out_stream.write(chunk)
 
-    sys.stderr.write(f"Final stream contained {dropped}/{total} frames and tag blocks not in all previous streams\n")
+    # TODO: always keep tag blocks?
+
+    sys.stderr.write(f"Final stream contained {dropped}/{total} frames and tag blocks not in all previous streams at coherent locations\n")
     sys.stderr.write(f"Result stream has {total - dropped} frames and tag blocks\n")
 
 
